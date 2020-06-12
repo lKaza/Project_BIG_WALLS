b6527501799c889a1d10fd70fb8c787c67dea960
lKaza <rodrigoguzman1202@gmail.com>
2020-06-12 16:00:34 -0400

    Snap en editor- con codigo en Gameobject

--- Assets/EditorSnap.cs
+++ Assets/EditorSnap.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class EditorSnap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [Range(1f, 20f)] [SerializeField] float gridSize=10f;
     void Update()
     {
-        
+        Vector3 snapPos;
+        snapPos.x = Mathf.Round(transform.position.x/gridSize)*gridSize;
+        snapPos.z = Mathf.Round(transform.position.z/gridSize)*gridSize;
+        transform.position = new Vector3 (snapPos.x,0f,snapPos.z);
     }
 }
