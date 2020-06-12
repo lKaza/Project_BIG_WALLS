ef371c8d2650a6acbe51d12b2f7cac92edba8970
lKaza <rodrigoguzman1202@gmail.com>
2020-06-12 16:41:19 -0400

    Textmesh en editor con posicion [SelectionBase] para no seleccionar otras estupideces

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
 {
+    TextMesh textMesh;
+
+    void Start(){
+        textMesh = GetComponentInChildren<TextMesh>();       
+    }
 
     [Range(1f, 20f)] [SerializeField] float gridSize=10f;
     void Update()
     {
+        
         Vector3 snapPos;
         snapPos.x = Mathf.Round(transform.position.x/gridSize)*gridSize;
         snapPos.z = Mathf.Round(transform.position.z/gridSize)*gridSize;
+        textMesh.text = snapPos.x/gridSize + "," +snapPos.z/gridSize;
         transform.position = new Vector3 (snapPos.x,0f,snapPos.z);
     }
 }
