bd340fc76052f79832836dce4975946b481ade91
lKaza <rodrigoguzman1202@gmail.com>
2020-06-14 15:36:23 -0400

    agregada explosion al morir enemigos -utilizando instancia al prefab no vinculado al GJ

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,6 +6,8 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] int HP = 2000;
     [SerializeField] Collider myCollider;
+    [SerializeField] ParticleSystem dmgFX;
+    [SerializeField] ParticleSystem deathFX;
     public bool isAlive = true;
     public GameObject enemyPrefab;
     
@@ -21,12 +23,14 @@ public class Enemy : MonoBehaviour
         HP -= dmg;
         if (HP <= 0)
         {
+            deathFX = Instantiate(deathFX,transform.position,Quaternion.identity);
+            deathFX.Play();
             Destroy(gameObject);
         }
        
     }
-
     private void OnParticleCollision(GameObject other) {
         TakeDMG(20);
+        dmgFX.Play();
     }
 }
