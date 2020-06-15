1be479251df66c31420699b4f526684a4a6e2c05
lKaza <rodrigoguzman1202@gmail.com>
2020-06-15 02:08:36 -0400

    fixeo de memito

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    
+    [SerializeField] float speed = 0.5f;
+    [SerializeField] ParticleSystem goalVFX;
+
     // Start is called before the first frame update
     void Start()
     {
         PathFinder pathfinder = FindObjectOfType<PathFinder>();
-        StartCoroutine(PrintWayPoints(pathfinder.getPath()));
+        StartCoroutine(FollowPath(pathfinder.getPath()));
     }
 
-    IEnumerator PrintWayPoints(List<Waypoint> path)
-    { 
+    IEnumerator FollowPath(List<Waypoint> path)
+    {
         foreach (Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;          
-            yield return new WaitForSeconds(1f);
-        }       
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(speed);
+        }
+        ReachGoal();
+    }
+
+    private void ReachGoal()
+    {
+        goalVFX = Instantiate(goalVFX, transform.position, Quaternion.identity);
+        goalVFX.Play();
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
